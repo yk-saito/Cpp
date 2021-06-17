bcd0b8cfa30c123756e50afeca809298a482c859
ysaito <shiva.lts239@gmail.com>
2021-06-17 16:48:30 +0900

    on the way Module01/ex07!

--- Cpp-Module/01/ex07/main.cpp
+++ Cpp-Module/01/ex07/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ysaito <ysaito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 20:55:54 by ysaito            #+#    #+#             */
-/*   Updated: 2021/06/16 23:48:06 by ysaito           ###   ########.fr       */
+/*   Updated: 2021/06/17 16:44:53 by ysaito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 
 int	main(int argc, char *argv[])
 {
@@ -40,13 +41,59 @@ int	main(int argc, char *argv[])
 		return (EXIT_FAILURE);
 	}
 	std::string		fileName = argv[1];
-	std::string		s1 = argv[2];
-	std::string		s2 = argv[3];
+	std::string		srcStr = argv[2];
+	std::string		replaceStr = argv[3];
 	std::fstream	file;
 
-	file.open(fileName, );
+	file.open(fileName, std::ios::in);
+	if (! file.is_open())
+	{
+		std::cerr << "ファイルが開けませんでした。" << std::endl;
+		return (EXIT_FAILURE);
+	}
 
+	std::string		buf; //fileの読み込みに使用
+	std::string		newFileName; //新しく作成するreplaceファイル名
+	std::fstream	newFile;	// replaceファイルを操作するfstreamのインスタンス
 
+	/*
+	新しいファイル名を作成
+	*/
+	std::string::size_type	posExt = 0; // postion extension
+	posExt = fileName.find(".", posExt);
+	// 拡張子なし
+	if (posExt == std::string::npos)
+	{
+		newFileName = fileName + ".replace";
+	}
+	// 拡張子あり
+	else
+	{
+		newFileName = fileName.substr(0, posExt) + ".replace";
+	}
 
+	newFile.open(newFileName, std::ios::out);
+	if (! newFile.is_open())
+	{
+		std::cerr << "新しいファイルが作成できませんでした。" << std::endl;
+		file.close();
+		return (EXIT_FAILURE);
+	}
+
+	while (std::getline(file, buf))
+	{
+		if (! buf.empty())
+		{
+			std::string::size_type	pos = 0;
+			while ((pos = buf.find(srcStr, pos)) != std::string::npos)
+			{
+				buf.replace(pos, srcStr.length(), replaceStr);
+				pos += replaceStr.length();
+			}
+		}
+		newFile << buf << std::endl;
+	}
+	file.close();// デストラクタで閉じてくれるみたい。あくまで明示的に書いた
+	newFile.close();
 	return (EXIT_SUCCESS);
 }
