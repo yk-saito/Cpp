0fdca208fd2537529f14956d0e89df6a7c1c5b5f
ysaito <shiva.lts239@gmail.com>
2021-10-19 00:19:13 +0900

    Module08:ex00 fix typename error.

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: ysaito <ysaito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 13:48:26 by ysaito            #+#    #+#             */
-/*   Updated: 2021/10/18 14:18:32 by ysaito           ###   ########.fr       */
+/*   Updated: 2021/10/19 00:18:16 by ysaito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,31 @@
 # define EASYFIND_HPP
 
 #include <algorithm>
+#include <exception>
 #include <vector>
 
-template <typename T>
-typename std::vector<T>::iterator	easyfind(const std::vector<T> vector , const int element)
-{
-	std::vector<T>::iterator	iterator;
-
-	iterator = std::find(vector.begin(), vector.end(), element);
-	if (iterator == vector.end())
-	{
-		throw ("easyfind(): Not found element.");
+class NotFoundException : public std::exception {
+	virtual const char* what() const throw() {
+		return ("Not found parameter...");
 	}
+};
+
+template <typename T>
+void	easyfind(const std::vector<T>& vector , const int parameter)
+{
+	typename std::vector<T>::size_type	index;
+
+	for (index = 0; index < vector.size(); index++)
+	{
+		if (vector[index] == parameter)
+		{
+			std::cout << "Found first parameter! vector["
+					  << index << "] = "
+					  << vector[index] << std::endl;
+			return ;
+		}
+	}
+	throw (NotFoundException());
 }
 
 #endif
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ysaito <ysaito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 13:48:18 by ysaito            #+#    #+#             */
-/*   Updated: 2021/10/18 14:14:33 by ysaito           ###   ########.fr       */
+/*   Updated: 2021/10/18 18:13:55 by ysaito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,30 @@
 
 int	main()
 {
-	std::vector<int>			vector_i = {1, 4, 2, 3, 2, 5};
-	std::vector<int>::iterator	iterator_i;
+	int					array_i[] = {1, 4, 2, 3, 2, 5};
+	std::vector<int>	vector_i(array_i, array_i + sizeof(array_i) / sizeof(int));
+
+	int					array_i_empty[] = {};
+	std::vector<int>	vector_i_empty(array_i_empty, array_i_empty + sizeof(array_i_empty) / sizeof(int));
 
 	try
 	{
-		iterator_i = easyfind(vector_i, 2);
+		::easyfind<int>(vector_i, 2);
+		::easyfind<int>(vector_i, 10);
 	}
-	catch(const std::string error_str)
+	catch(const std::exception& e)
 	{
-		std::cerr << error_str << std::endl;
+		std::cerr << e.what() << std::endl;
 	}
+
+	try
+	{
+		::easyfind<int>(vector_i_empty, 2);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
 	return (0);
 }
